90df63e312acef4eac42b552fce5c82e9bb4617c
Chelsea Voss <csvoss@mit.edu>
2014-12-10 10:39:52 -0500

    Stopping point, last night: svgs buggy; not draggable/droppable yet; no Ajax yet

--- circuits/views.py
+++ circuits/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
+import json
+
+from qutip import *
+from IPython.display import Image
 
 # Create your views here.
+def main(request):
+    
+    qc = QubitCircuit(10)
+    qc_svg = qc.svg
+
+    gate_svgs = get_gate_svgs()
+
+    circuit_init_svg = qc_svg._data
+    circuit_init_data = json.dumps("")
+
+    data = {
+        "gate_svgs": gate_svgs,
+        "circuit_init_svg": circuit_init_svg,
+        "circuit_init_data": circuit_init_data,
+    }
+    return TemplateResponse(request, "index.html", data)
+
+
+def get_gate_svgs():
+    output = []
+
+    ## Hadamard
+    q0 = QubitCircuit(1)
+    q0.add_gate("SNOT")
+    output.append(q0.svg._data)
+
+    ## CNOT
+    q1 = QubitCircuit(2)
+    q1.add_gate("CNOT", targets=1, controls=0)
+    output.append(q1.svg._data)
+
+    ## SWAP
+    q2 = QubitCircuit(2)
+    q2.add_gate("SWAP", targets=[0,1])
+    output.append(q2.svg._data)
+
+    return output
--- quantummechanic/settings.py
+++ quantummechanic/settings.py
@@ -80,3 +80,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+
+MEDIA_ROOT = PROJECT_PATH + "/media/"
+
+TEMPLATE_DIRS = (
+    PROJECT_PATH + '/templates/',
+)
