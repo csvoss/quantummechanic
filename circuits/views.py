85f6a1791a9933e8bc7e2354b29c9306bd1828ce
Chelsea Voss <csvoss@mit.edu>
2014-12-10 11:44:14 -0500

    Milestone: pretty PNG gates
    CSS working, centered, hoverable, not derpy -- everything!

--- circuits/views.py
+++ circuits/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.template.response import TemplateResponse
 
 import json
+import os
+import base64
 
 from qutip import *
 from IPython.display import Image
@@ -11,37 +13,58 @@ from IPython.display import Image
 def main(request):
     
     qc = QubitCircuit(10)
-    qc_svg = qc.svg
 
-    gate_svgs = get_gate_svgs()
+    gate_pngs = get_gate_pngs()
 
-    circuit_init_svg = qc_svg._data
+    circuit_init_svg = image_data(qc)
     circuit_init_data = json.dumps("")
 
     data = {
-        "gate_svgs": gate_svgs,
-        "circuit_init_svg": circuit_init_svg,
+        "gate_pngs": gate_pngs,
+        "circuit_init_png": circuit_init_svg,
         "circuit_init_data": circuit_init_data,
     }
     return TemplateResponse(request, "index.html", data)
 
 
-def get_gate_svgs():
+def get_gate_pngs():
+
+    ## Note: Some of these are single-qubit, some double-qubit.
+    ## Some take in special parameters.
+
     output = []
+
+
+    #RX
+    q3 = QubitCircuit(1)
+    q3.add_gate("RX", 0, None, pi/2, r"\theta")
+    output.append(image_data(q3))
 
     ## Hadamard
     q0 = QubitCircuit(1)
     q0.add_gate("SNOT")
-    output.append(q0.svg._data)
+    output.append(image_data(q0))
 
     ## CNOT
     q1 = QubitCircuit(2)
     q1.add_gate("CNOT", targets=1, controls=0)
-    output.append(q1.svg._data)
+    output.append(image_data(q1))
 
     ## SWAP
     q2 = QubitCircuit(2)
     q2.add_gate("SWAP", targets=[0,1])
-    output.append(q2.svg._data)
+    output.append(image_data(q2))
 
     return output
+
+
+def image_data(q):
+    image_tag = "<img src=\"data:image/png;base64,%s\"/>"
+    base_64_data = base64.b64encode(q.png.data)
+    return image_tag % base_64_data
+
+
+def refresh():
+    os.remove("qcirc.pdf")
+    os.remove("qcirc.png")
+    os.remove("qcirc.tex")
